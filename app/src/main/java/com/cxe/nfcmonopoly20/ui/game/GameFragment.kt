ced13f10859ac351d85932c0b7e32b1f44cfd004
Christos <cefstathiou13@gmail.com>
2022-04-15 22:39:52 +0300

    Keyboard does move buttons up now

--- app/src/main/java/com/cxe/nfcmonopoly20/ui/game/GameFragment.kt
+++ app/src/main/java/com/cxe/nfcmonopoly20/ui/game/GameFragment.kt
@@ -52,7 +52,6 @@ class GameFragment : Fragment() {
                     exitDialog()
                 }
             }
-
         requireActivity().onBackPressedDispatcher.addCallback(
             viewLifecycleOwner,
             onBackPressedCallback
